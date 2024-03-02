bc9b227f7095892bf2c901e7ba5120b5b5269e2f
nicccsss <155798224+nicccsss@users.noreply.github.com>
2024-03-02 14:43:47 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+function redirectTojump() {
+    document.body.classList.add('zoom-in');
+    window.location.href = 'jump.html';
+}
+
+function redirectTomouse   () {
+    document.body.classList.add('zoom-in');
+    window.location.href = 'mouse.html';
+}
+
+function redirectToindexani() {
+    document.body.classList.add('zoom-in');
+    window.location.href = 'indexani.html';
+}
+
+function redirectToresume() {
+    document.body.classList.add('zoom-in');
+    window.location.href = 'resume.html';
+}
+
+
 
 document.addEventListener("DOMContentLoaded", function() {
     // Get all the activity links
